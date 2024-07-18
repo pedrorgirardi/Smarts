91165e286a3f3a7b380ab68ab88ab6362de15794
pedrorgirardi <585191+pedrorgirardi@users.noreply.github.com>
2024-07-18 14:59:38 -0300

    Text Document Did Open WIP

--- language_server_client.py
+++ language_server_client.py
@@ -94,11 +94,12 @@ class LanguageServerClient:
         logger.debug("Send Worker is ready")
 
         while (message := self.send_queue.get()) is not None:
-            logger.debug(f"> {message}")
+            if request_id := message.get('id'):
+                logger.debug(f"> REQUEST {request_id} {message['method']}")
+            else:
+                logger.debug(f"> NOTIFICATION {message['method']}")
 
             try:
-                self.server_request_count += 1
-
                 content = json.dumps(message)
 
                 header = f"Content-Length: {len(content)}\r\n\r\n"
@@ -111,6 +112,8 @@ class LanguageServerClient:
                     logger.error(f"Can't write to server's stdin: {e}")
 
             finally:
+                self.server_request_count += 1
+
                 self.send_queue.task_done()
 
         # 'None Task' is complete.
@@ -207,6 +210,8 @@ class LanguageServerClient:
                 }
             )
 
+            # TODO: Send textDocument/didOpen
+
         # Enqueue 'initialize' message.
         # Message must contain "method" and "params";
         # Keys "id" and "jsonrpc" are added by the worker.
@@ -299,6 +304,35 @@ class LanguageServerClient:
             returncode = self.server_process.wait()
 
         logger.debug(f"Server terminated with returncode {returncode}")
+
+    def text_document_did_open(self):
+        # The document open notification is sent from the client to the server
+        # to signal newly opened text documents.
+        #
+        # The document’s content is now managed by the client
+        # and the server must not try to read the document’s content using the document’s Uri.
+        #
+        # Open in this sense means it is managed by the client.
+        # It doesn’t necessarily mean that its content is presented in an editor.
+        #
+        # An open notification must not be sent more than once without a corresponding close notification send before.
+        # This means open and close notification must be balanced and the max open count for a particular textDocument is one.
+        #
+        # https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_didOpen
+        self.send_queue.put(
+            {
+                "jsonrpc": "2.0",
+                "method": "textDocument/didOpen",
+                "params": {
+                    "textDocument": {
+                        "uri": "",
+                        "languageId": "",
+                        "version": "",
+                        "text": "",
+                    },
+                },
+            }
+        )
 
 
 # -- INPUT HANDLERS
