bec1200b12b35c87ce5f8c858d9564a2276061a9
pedrorgirardi <585191+pedrorgirardi@users.noreply.github.com>
2026-01-27 19:05:57 -0300

    Logs

--- lib/smarts_client.py
+++ lib/smarts_client.py
@@ -1442,7 +1442,7 @@ class LanguageServerClient:
             self._server_status = LanguageServerStatus.INITIALIZING
             self._server_process = server_process
 
-        self._logger.info(f"Initializing {self._name} ({self._server_process.pid}) ⏳")
+        self._logger.info(f"Initializing {self._name} ({self._server_process.pid})...")
 
         # Thread responsible for handling received messages.
         self._handler = threading.Thread(
